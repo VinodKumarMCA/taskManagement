93d2d2170eefa1fb35dcb6edb6db4c6ca207281c
VinodKumar R <rvinothkumarbca@gmail.com>
2015-04-24 12:06:16 -0700

    Update taskManagement.js

--- com.vino.forKslHindu.taskManagement/WebContent/theme/js/taskManagement.js
+++ com.vino.forKslHindu.taskManagement/WebContent/theme/js/taskManagement.js
@@ -1,4 +1,16 @@
 function taskViewOptionChange()
 {
-	
+	alert('Task Filtered.');
 }
+function validateForm() {
+    var x = document.forms["taskManagementform"]["task"].value;
+    if (x == null || x == "") {
+        alert("Task Name must be filled out");
+        return false;
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+	document.forms["contact-form"].addEventListener("submit",postData);
+});
+
